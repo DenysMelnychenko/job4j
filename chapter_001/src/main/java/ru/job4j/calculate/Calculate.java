07cf738d620af15f3acfceee17ddba9689a6db39
DenysMelnychenko <aspirant89@gmail.com>
2018-06-06 11:44:43 +0300

    Created new directiry for tests and added new TestCalculate.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -9,6 +9,15 @@ package ru.job4j.calculate;
 public class Calculate {
 	
 	/**
+    * Method echo.
+    * @param name Your name.
+    * @return Echo plus your name.
+	*/
+	public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+	}
+	
+	/**
 	*Main.
 	*@param args - args
 	*/
@@ -16,4 +25,6 @@ public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+	
+	
 }
