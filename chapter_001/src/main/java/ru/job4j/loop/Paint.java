1d85a1e02364a16cbb51f4daa4d9203ccb1d9928
DenysMelnychenko <aspirant89@gmail.com>
2018-07-13 18:26:49 +0300

    added Paint and PaintTest classes according to the task. Addone more test method to check pyramid method.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,69 @@
+package ru.job4j.loop;
+
+/**
+ * @author Denis Melnichenko (mailto:aspirant89@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+
+    /**
+     * @param height value of pyramid rows.
+     * @return The String that represent the pyramid.
+     */
+    public String rightTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * @param height value of pyramid rows.
+     * @return The String that represent the pyramid.
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= weight - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+
+    /**
+     * @param height value of pyramid rows.
+     * @return the String that represent the pyramid.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
