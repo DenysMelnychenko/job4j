f0b5aadda2533aa47d90b07e77b763d779ae4ebe
DenysMelnychenko <aspirant89@gmail.com>
2018-08-02 23:34:31 +0300

    Added BubbleSort and BubbleSortCheck classes according to the task.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,12 +3,16 @@ package ru.job4j.array;
 public class BubbleSort {
 
     public int[] sort(int[] array) {
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
+        int tmp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
             }
         }
-      return new int[]{0};
+        return array;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,19 @@
+
+
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BubbleSortTest {
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] example = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] result = bubbleSort.sort(example);
+        assertThat(result, is(new int[]{0, 1, 1, 2, 3, 4, 5, 5, 7, 8}));
+    }
+}
+
