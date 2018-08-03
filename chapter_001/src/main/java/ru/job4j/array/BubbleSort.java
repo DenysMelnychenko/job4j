639befb7589de09fcb3a16722bceefaa41401774
denysmelnychenko <aspirant89@gmail.com>
2018-08-03 14:19:05 +0300

    Added private method swap to BubbleSort class.

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,16 +3,21 @@ package ru.job4j.array;
 public class BubbleSort {
 
     public int[] sort(int[] array) {
-        int tmp;
+
         for (int i = 0; i < array.length; i++) {
             for (int j = 1; j < array.length - i; j++) {
                 if (array[j - 1] > array[j]) {
-                    tmp = array[j - 1];
-                    array[j - 1] = array[j];
-                    array[j] = tmp;
+                    swap(array, j);
                 }
             }
         }
         return array;
+    }
+
+    private void swap(int[] array, int j) {
+        int tmp;
+        tmp = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = tmp;
     }
 }
