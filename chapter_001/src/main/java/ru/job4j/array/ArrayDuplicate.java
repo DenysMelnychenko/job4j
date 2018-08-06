6f8d6e28b467ac70bb73c03b43138c0d9db83a3f
denysmelnychenko <aspirant89@gmail.com>
2018-08-06 14:39:37 +0300

    For homework task.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+
+    public String[] remove(String[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].equals(array[i + 1])) {
+                Arrays.copyOf()
+            }
+
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate example =new ArrayDuplicate();
+        String[] result = example.remove(new String[]{"Good", "Very", "Night", "Very", "Much"});
+        assertThat(result,is (new String[]{"Good", "Very", "Night", "Much"}));
+
+    }
+}
