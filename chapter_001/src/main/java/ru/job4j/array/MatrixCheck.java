41f06a80020bc8bd3b1b88527701dc0aaf53c059
denysmelnychenko <aspirant89@gmail.com>
2018-08-07 13:42:56 +0300

    Changed realisation of mono method and added javadoc to the MatrixCheck class.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,13 +1,22 @@
 package ru.job4j.array;
 
+/**
+ * @author Denis Melnichenko (mailto:aspirant89@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class MatrixCheck {
+
+    /**
+     * @param data array that will checked
+     * @return result of checking
+     */
     public boolean mono(boolean[][] data) {
         boolean result = true;
         for (int i = 0; i < data.length - 1; i++) {
-            for (int j = 0; j < data.length - 1; j++) {
-                if (data[i][j] != data[i + 1][j + 1]) {
-                    result = false;
-                }
+
+            if (data[i][i] != data[i + 1][i + 1]) {
+                result = false;
             }
         }
         return result;
